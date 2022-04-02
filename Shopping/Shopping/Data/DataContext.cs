45086d4dee71fd470ecd4618719bc2860cc8ef96
AdrianGaray <adriangaray396@gmail.com>
2022-04-02 13:09:46 -0300

    Initial Project Shopping

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        // OnModelCreating, se ejecuta antes de crear la BD.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Se crea un indice sobre la columna name, porque no puede haber dos paises con el mismo nombre.
+            // la c, es de configuracion.
+            // le estamos diciendo que cree un indice unico sobre el campo name.
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
